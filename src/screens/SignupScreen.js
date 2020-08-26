7ca6992dd019a7c441c35222ad8cd431312ba576
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-26 09:12:26 +0200

    completed lesson 198: included react-native-elements & replaced elemnts with react-native-elements for styling

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import { color } from 'react-native-reanimated';
 
 const SignupScreen = ({ navigation }) => {
     return (
         <>
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-            <Button title="Go to signin" onPress={() => navigation.navigate('Signin')} />
-            <Text style={{ height: 2 }} />
-            <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')} />
+            <Spacer>
+                <Text h4 style={styles.title}>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label="Email" />
+            <Spacer />
+            <Input label="Password" />
+            <Spacer>
+                <Button title="Sign Up" onPress={() => navigation.navigate('Signin')} />
+            </Spacer>
         </>
     );
 }
 
 const styles = StyleSheet.create({
-
+    title: {
+        textAlign: 'center',
+        color: 'dodgerblue'
+    }
 })
 
 export default SignupScreen;
