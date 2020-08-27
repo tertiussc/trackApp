18858d17fe93028acdca1e5c8006073c842c31d7
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-27 08:30:44 +0200

    completed lesson 201: added state to email and password inputs

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 import { color } from 'react-native-reanimated';
 
+
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <>
+        <View style={styles.container}>
             <Spacer>
                 <Text h4 style={styles.title}>Sign Up for Tracker</Text>
             </Spacer>
-            <Input label="Email" />
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
             <Spacer />
-            <Input label="Password" />
+            <Input
+                secureTextEntry
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
             <Spacer>
                 <Button title="Sign Up" onPress={() => navigation.navigate('Signin')} />
             </Spacer>
-        </>
+        </View>
     );
+}
+
+SignupScreen.navigationOptions = () => {
+    return {
+        header: () => false
+    }
 }
 
 const styles = StyleSheet.create({
     title: {
         textAlign: 'center',
         color: 'dodgerblue'
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200,
     }
 })
 
