579ec2138900a4dfdf78c944d76393db43ed82cf
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-31 08:57:56 +0200

    coppleted lesson 203: setup context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,29 @@ const authReducer = (state, action) => {
     }
 }
 
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // Make api request to signup
+        // If sign up then update state with authenticated
+        // If error show error message
+    }
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // If sign in update state to authenticated
+        // If fail show error
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        // Just sign out
+    }
+}
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signup, signin, signout },
     { isSignedIn: false }
 )
