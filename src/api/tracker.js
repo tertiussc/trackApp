24ebf66ddf19d99d504fad00dfe70753172f5f9d
Tertius Scheepers <Tertiussc@gmail.com>
2020-08-31 10:40:16 +0200

    completed lesson 205: Signup via API

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'http://29d18bea706c.ngrok.io'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext'
+import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
     switch (action.type) {
@@ -8,10 +9,13 @@ const authReducer = (state, action) => {
 }
 
 const signup = (dispatch) => {
-    return ({ email, password }) => {
-        // Make api request to signup
-        // If sign up then update state with authenticated
-        // If error show error message
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', { email, password })
+            console.log(response.data)
+        } catch (err) {
+            console.log(err.response.data)
+        }
     }
 };
 
