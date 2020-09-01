f02ab13f16793e2d915bccdd3cc73db8654982f9
Tertius Scheepers <Tertiussc@gmail.com>
2020-09-01 10:16:02 +0200

    completed lesson 206: Signup error handling

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,6 +3,8 @@ import trackerApi from '../api/tracker'
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload }
         default:
             return state
     }
@@ -14,7 +16,7 @@ const signup = (dispatch) => {
             const response = await trackerApi.post('/signup', { email, password })
             console.log(response.data)
         } catch (err) {
-            console.log(err.response.data)
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
         }
     }
 };
@@ -35,5 +37,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signup, signin, signout },
-    { isSignedIn: false }
+    { isSignedIn: false, errorMessage: '' }
 )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -32,6 +32,7 @@ const SignupScreen = ({ navigation }) => {
                 autoCapitalize='none'
                 autoCorrect={false}
             />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
             <Spacer>
                 <Button title="Sign Up" onPress={() => signup({ email, password })} />
             </Spacer>
@@ -54,6 +55,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 200,
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15,
+        marginTop: 15
     }
 })
 
