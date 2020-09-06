96de8deccd1f02af1ae58ac73c3bd38467a2f9cf
Tertius Scheepers <Tertiussc@gmail.com>
2020-09-06 09:12:37 +0200

    completed lesson 223: local signin

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationEvents } from 'react-navigation'
 import { Text } from 'react-native-elements'
@@ -10,8 +10,11 @@ import NavLink from '../components/NavLink'
 
 
 const SignupScreen = ({ navigation }) => {
-    const { state, signup, clearErrorMessage } = useContext(AuthContext)
+    const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext)
 
+    useEffect(() => {
+        tryLocalSignin()
+    }, [])
 
     return (
         <View style={styles.container}>
