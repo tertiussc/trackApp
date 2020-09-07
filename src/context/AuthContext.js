a9e7ee192fc39f094776c91f68dc2995e18c0aa9
Tertius Scheepers <Tertiussc@gmail.com>
2020-09-07 08:28:07 +0200

    completed lesson 225: Signout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
             return { errorMessage: '', token: action.payload }
         case 'clear_error_message':
             return { ...state, errorMessage: '' }
+        case 'signout':
+            return { token: null, errorMessage: '' }
         default:
             return state
     }
@@ -57,10 +59,10 @@ const signin = (dispatch) => async ({ email, password }) => {
 }
 
 
-const signout = (dispatch) => {
-    return () => {
-        // Just sign out
-    }
+const signout = (dispatch) => async () => {
+    await AsyncStorage.removeItem('token')
+    dispatch({ type: 'signout' })
+    navigate('Signup')
 }
 
 export const { Provider, Context } = createDataContext(
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const AccountScreen = () => {
+    const { signout } = useContext(AuthContext)
+
     return (
-        <View>
-            <Text style={{ fontSize: 48 }}>AccountScreen</Text>
-        </View>
+        <>
+            <Text style={{ fontSize: 48, marginTop: 50, marginLeft: 15 }}>Sign out Screen</Text>
+            <Spacer>
+                <Button title="Sign Out" onPress={signout} />
+            </Spacer>
+        </>
     );
 }
 
