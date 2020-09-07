6cc6d6900b5b921ab04d2f0c9d712d4ca7a10158
Tertius Scheepers <Tertiussc@gmail.com>
2020-09-07 10:21:24 +0200

    completed lesson 230: Added Map Polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, { Polyline } from 'react-native-maps'
 
 
 const Map = () => {
+    let points = []
+    for (let i = 0; i < 10; i++) {
+        if (i % 2 === 0) {
+            points.push({
+                latitude: 37.33233 + i * 0.001,
+                longitude: -122.03121 + i * 0.0001,
+            })
+        } else {
+            points.push({
+                latitude: 37.33233 - i * 0.0002,
+                longitude: -122.03121 + i * 0.0002,
+            })
+        }
+    }
+
     return (
         <MapView
             style={styles.map}
@@ -13,7 +28,9 @@ const Map = () => {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01
             }}
-        />
+        >
+            <Polyline coordinates={points} />
+        </MapView>
     );
 }
 
