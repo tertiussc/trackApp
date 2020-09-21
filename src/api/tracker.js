12a8eb3af1b532802b34acf3e8e1d3fdd95fbc71
Tertius Scheepers <Tertiussc@gmail.com>
2020-09-21 08:11:51 +0200

    completed lesson 260: Automatic authentication for API calls via axios

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from 'axios'
+import { AsyncStorage } from 'react-native'
 
-export default axios.create({
-    baseURL: 'http://c433b695ec18.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://3e6c53d1296f.ngrok.io'
 });
+
+instance.interceptors.request.use(
+    // First function is for success
+    async (config) => {
+        const token = await AsyncStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    // Second function is for ANY errors
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance;
