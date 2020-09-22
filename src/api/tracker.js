e9abf581f2988e342f58474c1d4f3979aab581db
Tertius Scheepers <Tertiussc@gmail.com>
2020-09-22 09:35:18 +0200

    completed lesson 265: pass info to Track details

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { AsyncStorage } from 'react-native'
 
 const instance = axios.create({
-    baseURL: 'http://3e6c53d1296f.ngrok.io'
+    baseURL: 'http://3927bd582cd8.ngrok.io'
 });
 
 instance.interceptors.request.use(
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements'
+import React, { useContext } from 'react';
+import { FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements'
 import Spacer from '../components/Spacer'
+import { NavigationEvents, navigationEvents } from 'react-navigation'
+import { Context as TrackContext } from '../context/TrackContext'
 
 const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext)
+    console.log(state)
     return (
-        <Spacer>
-            <Text style={{ fontSize: 48, marginBottom: 20 }}>TrackListScreen</Text>
-            <Button title="Go to track detail" onPress={() => navigation.navigate('TrackDetail')} />
-        </Spacer>
+        <>
+            <NavigationEvents onWillFocus={fetchTracks} />
+            <Spacer>
+                <Text style={{ fontSize: 48, marginBottom: 20 }}>TrackListScreen</Text>
+                <FlatList
+                    data={state}
+                    keyExtractor={item => item._id}
+                    renderItem={({ item }) => {
+                        return <TouchableOpacity onPress={() =>
+                            navigation.navigate('TrackDetail', { _id: item._id })
+                        }>
+                            <ListItem chevron title={item.name} />
+                        </TouchableOpacity>
+                    }}
+                />
+            </Spacer>
+        </>
     );
 }
 
